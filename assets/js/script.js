58c25fae79bf2e70d4e15fbd3a94012ff03fb751
Anders <1086088@ucn.dk>
2021-06-17 20:37:34 +0200

    Followed the video. Time to make some custom stuff.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,128 @@
 //JS by Anders Erik Nissen
 //UCN MMDA0920
 
+const template = document.createElement("template");
+template.innerHTML = '<style>h3 {color: red;}</style><section class="profil_info"><div><h3></h3><article id="info"><p><slot name="email" /></p><p>TLF</p></article><button id="toggle-info">HIDE INFO</button></div><article></article></section>';
 
-console.log("               ____________                _____________________________    ________            _______")
-console.log("              /            \\              |                             |  |         \\         |       |")
-console.log("             /              \\             |                             |  |          \\        |       |");
-console.log("            /                \\            |         ____________________|  |           \\       |       |")
-console.log("           /        /\\        \\           |        |                       |            \\      |       |")
-console.log("          /        /  \\        \\          |        |                       |             \\     |       |")
-console.log("         /        /    \\        \\         |        |____________________   |              \\    |       |")
-console.log("        /        /      \\        \\        |                             |  |               \\   |       |")
-console.log("       /        /________\\        \\       |                             |  |       |\\       \\  |       |")
-console.log("      /                            \\      |         ____________________|  |       | \\       \\ |       |")
-console.log("     /        _____________         \\     |        |                       |       |  \\       \\|       |")
-console.log("    /        /             \\         \\    |        |                       |       |   \\               |")
-console.log("   /        /               \\         \\   |        |____________________   |       |    \\              |")
-console.log("  /        /                 \\         \\  |                             |  |       |     \\             |")
-console.log(" /        /                   \\         \\ |                             |  |       |      \\            |")
-console.log("/________/                     \\_________\\|_____________________________|  |_______|       \\___________|")
+class ProfilInfo extends HTMLElement {
+    constructor() {
+        super();
+
+        this.showInfo = true;
+
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
+        // this.shadowRoot.querySelector("img").src = this.getAttribute("name");
+    }
+
+    toggleInfo() {
+        //Pisse smart. Den skifter i mellem true/false. Første gang sættes den til false, fordi i constructor er den true, så den gemmer indholdet første gang man trykker.
+        this.showInfo = !this.showInfo;
+
+
+
+        const info = this.shadowRoot.querySelector("#info");
+        const toggleBtn = this.shadowRoot.querySelector("#toggle-info");
+        
+        if(this.showInfo) {
+            info.style.display = "block";
+            toggleBtn.innerText = 'Hide Info'
+        } else {
+            info.style.display = "none";
+            toggleBtn.innerText = 'Show Info'
+
+        }
+    }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector("#toggle-info").addEventListener("click", () => this.toggleInfo())
+    }
+    disconnectedCallback () {
+        this.shadowRoot.querySelector("#toggle-info").removeEventListener("click", () => this.toggleInfo())
+    }
+}
+
+window.customElements.define('profil-info', ProfilInfo);
+
+
+
+
+
+
+
+
+
+// console.log()
+
+//        /\
+//       /  \
+//      / /\ \
+//     / /__\ \
+//    / /____\ \
+//   /_/      \_\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("               ____________                _____________________________    ________            _______")
+// console.log("              /            \\              |                             |  |         \\         |       |")
+// console.log("             /              \\             |                             |  |          \\        |       |");
+// console.log("            /                \\            |         ____________________|  |           \\       |       |")
+// console.log("           /        /\\        \\           |        |                       |            \\      |       |")
+// console.log("          /        /  \\        \\          |        |                       |             \\     |       |")
+// console.log("         /        /    \\        \\         |        |____________________   |              \\    |       |")
+// console.log("        /        /      \\        \\        |                             |  |               \\   |       |")
+// console.log("       /        /________\\        \\       |                             |  |       |\\       \\  |       |")
+// console.log("      /                            \\      |         ____________________|  |       | \\       \\ |       |")
+// console.log("     /        _____________         \\     |        |                       |       |  \\       \\|       |")
+// console.log("    /        /             \\         \\    |        |                       |       |   \\               |")
+// console.log("   /        /               \\         \\   |        |____________________   |       |    \\              |")
+// console.log("  /        /                 \\         \\  |                             |  |       |     \\             |")
+// console.log(" /        /                   \\         \\ |                             |  |       |      \\            |")
+// console.log("/________/                     \\_________\\|_____________________________|  |_______|       \\___________|")
 
 
 
